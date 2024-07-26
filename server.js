27a51e78e00d191190bb65d3f567a58a51652d04
Pedro K. L. <pedro.karasudani@hotmail.com>
2024-07-25 22:33:36 -0300

    post intregrando com o banco de dados

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
-
-const users = []; // simula banco de dados
 
 app.get('/usuarios', (req, res) => {
   res.status(200).json(users);
 });
 
-app.post('/usuarios', (req, res) => {
-  users.push(req.body);
-  res.status(201).json(users);
+app.post('/usuarios', async (req, res) => {
+  await prisma.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age,
+    },
+  });
+  res.status(201).json(req.body);
 });
 
 app.listen(3000);
